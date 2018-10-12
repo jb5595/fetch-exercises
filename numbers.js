10a37468557291d3ba837c4b406a5e48853e3a72
jb5595 <jacobberman1995@gmail.com>
2018-10-12 14:25:39 -0400

    Done.

--- numbers.js
+++ numbers.js
@@ -1,1 +1,98 @@
 // Add your code for the numbers API here!
+document.addEventListener('DOMContentLoaded', () => {
+setInterval(yearFacts,5000)
+document.querySelector("#number-one").addEventListener("click",factAbout1)
+document.querySelector("#pick-a-number").addEventListener("keyup",randomNumberFact)
+document.querySelector("#all-numbers-button").addEventListener("click", allTheNumbers)
+});
+
+// When the page loads, start an interval:
+// Every 5 seconds, fetch a fact about a year and show it on the screen in the #year-history div
+// Start with this year
+// Every 5 seconds, get the fact about the previous year
+let year = 2018
+
+function allTheNumbers(){
+  let url = "http://numbersapi.com/1..1000"
+  fetch(url)
+  .then(res => res.json())
+  .then(json => displayAllTheNumbers(json))
+}
+
+function displayAllTheNumbers(json) {
+  let parentNode = document.querySelector("#all-the-numbers")
+  while (parentNode.firstChild) {
+    parentNode.removeChild(parentNode.firstChild);
+  }
+  for(number in json){
+    let headerNode = document.createElement("h4")
+    let node = document.createElement("p")
+    parentNode.appendChild(headerNode)
+    parentNode.appendChild(node)
+    headerNode.innerText = `${number}:`
+    node.innerText = `${json[number]}`
+  }
+
+}
+
+function yearFacts(){
+  let url = `http://numbersapi.com/${year}/year`
+    fetch(url)
+    .then(res => res.text())
+    .then(text => displayYearFact(text, year--))
+}
+function displayYearFact(text, year) {
+
+  let parentNode = document.querySelector("#year-history")
+  let headerNode = document.createElement("h4")
+  let node = document.createElement("p")
+  parentNode.appendChild(headerNode)
+  parentNode.appendChild(node)
+  headerNode.innerText = `${year}:`
+  node.innerText = text
+}
+
+
+function factAbout1(){
+
+  let url =  "http://numbersapi.com/1"
+  fetch(url)
+  .then(res => res.text())
+  .then(text => displayFactAbout1(text))
+  // .then(json => displayFactAbout1(json))
+}
+
+function randomNumberFact(event){
+  if (isNaN(event.key) && event.key != "Backspace"){
+    alert("Number Inputs Only")
+  }
+  else{
+    let userInput = event.target.value
+    if (userInput != ""){
+      let url = `http://numbersapi.com/${userInput}`
+      fetch(url)
+      .then(res =>res.text())
+      .then(text =>displayRandomNumberFact(text))
+    }
+  }
+}
+
+function displayRandomNumberFact(text){
+  let parentNode = document.querySelector("#random-math-fact")
+  let factNode = document.createElement("p")
+  if (parentNode.firstChild) {
+    parentNode.removeChild(parentNode.firstChild);
+  }
+  parentNode.appendChild(factNode)
+  factNode.innerHTML = text;
+}
+
+function displayFactAbout1(text){
+  let parentNode = document.querySelector("#one-facts")
+  let factNode = document.createElement("p")
+  if (parentNode.firstChild) {
+    parentNode.removeChild(parentNode.firstChild);
+  }
+  parentNode.appendChild(factNode)
+  factNode.innerHTML = text;
+}
--- swapi.js
+++ swapi.js
@@ -1,1 +1,106 @@
 // Add your code for SWAPI here!
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector("#crawlBtn").addEventListener("click", openingCrawl)
+  document.querySelector("#planetForm").addEventListener("submit", planetSearch)
+  displayDroids()
+});
+
+function openingCrawl(){
+  fetch('https://swapi.co/api/films/1/')
+  .then(res => res.json())
+  .then(json => displayOpeningCrawl(json))
+}
+
+function displayOpeningCrawl(json){
+  let parentNode = document.querySelector("#crawlDiv")
+  if (parentNode.firstChild) {
+    parentNode.removeChild(parentNode.firstChild);
+  }
+  let crawlNode = document.createElement("p")
+  parentNode.appendChild(crawlNode)
+  crawlNode.innerHTML = json["opening_crawl"]
+}
+
+function planetSearch(event) {
+  event.preventDefault()
+  let userInput = event.target.querySelector("#planetInput").value
+  if (userInput < 1 || userInput >60 || isNaN(userInput)){
+    alert("Please Input Number Between 1 and 60")
+  }
+  let url = `https://swapi.co/api/planets/${userInput}/`
+  fetch(url)
+  .then(res => res.json())
+  .then(json => displayPlanetInfo(json))
+}
+
+function displayPlanetInfo(json){
+  let parentNode = document.querySelector("#planetData")
+  let planetNode = document.createElement("ul")
+  parentNode.appendChild(planetNode)
+  planetNode.innerHTML = `
+  <li>Name: ${json.name}</li>
+  <li>Climate ${json.climate}</li>`
+}
+
+function displayDroids(){
+  let baseUrl = "https://swapi.co/api/people/"
+  getDroid(baseUrl+"2/")
+  getDroid(baseUrl+"3/")
+
+}
+
+function getDroid(url){
+  fetch(url)
+  .then(res => res.json())
+  .then(json => displayDroid(json))
+}
+
+function displayDroid(json){
+  let parentNode = document.querySelector("#droidData")
+  let planetNode = document.createElement("ul")
+  parentNode.appendChild(planetNode)
+
+  planetNode.innerHTML = `
+  <li>Name: ${json.name}</li>
+  <li>Height ${json.height}</li>
+  <li>Weight ${json.mass}</li>
+  <button id = '${json.name}-button' data-homeworld = '${json.homeworld}' data-name = '${json.name}' >Show Homeworld Details</button>
+  <div id = '${json.name}-homeworld-details'></div>`
+  document.querySelector(`#${json.name}-button`).addEventListener("click",showHomeworldDetails)
+}
+
+function showHomeworldDetails(event){
+  let url = event.target.dataset.homeworld
+  fetch(url)
+  .then(res => res.json())
+  .then(json => displayHomeworldDetails(json, event))
+  }
+
+
+function displayHomeworldDetails(json, event){
+  let characterName = event.target.dataset.name
+  let parentNode = document.querySelector(`#${characterName}-homeworld-details`)
+  let planetNode = document.createElement("ul")
+  if (parentNode.firstChild) {
+    parentNode.removeChild(parentNode.firstChild);
+  }
+  parentNode.appendChild(planetNode)
+  planetNode.innerHTML = `
+  <h7>Homeworld Info </h7>
+  <li>Name: ${json.name}</li>
+  <li>Climate ${json.climate}</li>`
+}
+
+
+// When the page loads, fetch the data for the characters C-3P0 (id: 2) and R2-D2 (id: 3)
+// Show each droid's name, height, and mass on the screen in the #droidData
+//
+// Display a button for each droid that reads 'Show Homeworld Details'
+//
+// On click, this button should fetch to the api again to get information about the planet
+//
+// Things to consider:
+//
+// What information do you need from the first fetch to make the second?
+// How can you store this data on the page (without the user seeing it)?
+// When should you add the event listener for these two buttons?
